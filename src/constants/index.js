299c95547350a16852d6bbb95428aa2b4c5ee31e
Oscar Sánchez <ojsm45@gmail.com>
2021-05-19 08:33:12 -0300

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,11 @@
 import qs from 'query-string';
 
-const TS = '1585606761';
-const HASH = '6a04114df1d8f6784ad025a9c0eac182';
-const API_URL = 'https://gateway.marvel.com/v1/public';
-const API_KEY = '342d0588ed80d81fee3f32ce4bd8c85d';
+//const TS = '1585606761';
+/* Create hash*/
+//const HASH = '';
+//const API_URL = 'https://gateway.marvel.com/v1/public';
+/* Add your api key*/
+//const API_KEY = '';
 const AUTH_QUERIES = qs.stringify({
   ts: TS,
   apikey: API_KEY,
